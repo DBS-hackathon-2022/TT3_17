06881f12727e66b163a78472838a93c2d716cde4
blodvan1 <34994136+blodvan1@users.noreply.github.com>
2022-01-20 14:21:24 +0800

    REST API default without filters done

--- backend/backend/socialmedia/admin.py
+++ backend/backend/socialmedia/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import (USER, POST, LIKED_POST,POST_COMMENT)
+from .models import (USER, POST, LIKED_POST,POST_COMMENT, LIKED_POST2,POST_COMMENT2)
 
 # Register your models here.
 admin.site.register(USER)
 admin.site.register(POST)
 admin.site.register(LIKED_POST)
 admin.site.register(POST_COMMENT)
+admin.site.register(LIKED_POST2)
+admin.site.register(POST_COMMENT2)
--- backend/backend/socialmedia/serializers.py
+++ backend/backend/socialmedia/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import (USER, POST, LIKED_POST,POST_COMMENT)
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
 	class Meta:
 		model = USER
 		fields = ("User_ID", "Name","Age", "Birthday","Email","Phone","City","Country")
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(ModelSerializer):
 	class Meta:
 			model = POST
 			fields = ("Post_ID", "Post_Title","Post_Description","Post_image")
 
-class LikedPostSerializer(serializers.HyperlinkedModelSerializer):
+
+
+class LikedPostSerializer(ModelSerializer):
 	class Meta:
 			model = LIKED_POST
-			fields = ("User_ID", "Post_ID")
+			fields = ("User_ID",
+			 "Post_ID")
 
-class PostCommentSerializer(serializers.HyperlinkedModelSerializer):
+
+class PostCommentSerializer(ModelSerializer):
+	#User_ID = UserSerializer(many=False, read_only=True)
 	class Meta:
 			model = POST_COMMENT
 			fields = ("Comment_ID", "User_ID","Post_ID","Comment")
