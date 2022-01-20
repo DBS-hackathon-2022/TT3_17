7afff05385395a80ec6bb2e9fec2a01a762ce00e
jasxmine <jasmine.toh.2018@sis.smu.edu.sg>
2022-01-20 13:44:53 +0800

    Add posts functionality

--- frontend/src/Components/NewPost.jsx
+++ frontend/src/Components/NewPost.jsx
@@ -1,27 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect,useState} from 'react';
 
 
 import {Paper, TextField} from '@mui/material';
 import Button from '@mui/material/Button';
 // import SendIcon from '@mui/icons-material/Send';
-class Post extends Component{
+function Post({stateChanger, ...rest}){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const addPost = () => {
+        //call API w the input to add post
+        console.log(title, description) 
+        stateChanger(title)
+       
+    };
 
-    render(){
-        return(
-            <div className="row">
-                <Paper>
-                    <TextField placeholder="Describe what you are feeling"
+    return(
+        <div className="row">
+            <Paper>
+                <TextField placeholder="Post Title"
+                    value={title} 
+                    onInput={e => setTitle(e.target.value)}
+                    id="post" name="post" rows={1} fullWidth={true} style={{paddingBottom: 5}} />
+
+                <TextField placeholder="Describe what you are feeling"
+                    value={description} 
+                    onInput={e => setDescription(e.target.value)}
                     id="post" name="post" rows={2} fullWidth={true} multiline={true}/>
-                    <div style={{padding:10}} >
-                        {/* <IconButton><SendIcon/></IconButton> */}
-                        <Button variant="contained">
-                            POST
-                        </Button>
-                    </div>
-                </Paper>
-            </div>
-        )
-    }
+                <div style={{padding:10}} >
+
+                    <Button variant="contained" onClick={() => addPost()}>
+                        POST
+                    </Button>
+                </div>
+            </Paper>
+        </div>
+    )
+    
 
 }
 
